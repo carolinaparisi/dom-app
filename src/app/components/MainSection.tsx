76fae4229b41b6330cae521aac19b2d76258da70
Cássio Freitas <cassio.rsfreitas@gmail.com>
2024-08-31 22:05:37 +0100

    fix: decrease line heigh from MainSection subtitles

--- src/app/components/MainSection.tsx
+++ src/app/components/MainSection.tsx
@@ -1,22 +1,22 @@
 export default function MainSection() {
-	return (
-		<div className="flex flex-col gap-3 text-black leading-tight">
-			<div className="font-medium text-4xl leading-none">
-				<div>WELCOME</div>
-				<div>TO</div>
-				<div>DOM</div>
-			</div>
-			<div className="text-xl">
-				<div>Meeting Vote Room</div>
-				<div>created by Café com Letras</div>
-			</div>
-			<input
-				id="name"
-				name="name"
-				type="text"
-				placeholder="Your name"
-				className="block w-full rounded-s border-0 py-4 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray_soft placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-			/>
-		</div>
-	);
+  return (
+    <div className="flex flex-col gap-3 text-black leading-tight">
+      <div className="font-medium text-4xl leading-none">
+        <div>WELCOME</div>
+        <div>TO</div>
+        <div>DOM</div>
+      </div>
+      <div className="text-xl">
+        <div>Meeting Vote Room</div>
+        <div className="mt-[-4px]">created by Café com Letras</div>
+      </div>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        placeholder="Your name"
+        className="block w-full rounded-s border-0 py-4 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray_soft placeholder:text-gray focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+      />
+    </div>
+  );
 }
