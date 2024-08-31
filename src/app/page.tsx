0836ae5b3d8d9227c8c1c27acbcbcba762e40f63
Carolina Parisi <carolinaparisi5@gmail.com>
2024-08-31 21:13:03 +0100

    fix: add object-fit cover to the Image next component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 	return (
 		<div className="h-screen">
 			<div className="h-1/2 relative">
-				<Image alt="" src={background} fill />
+				<Image alt="" src={background} fill objectFit="cover" />
 			</div>
 			<div className="flex flex-col justify-between p-5 h-1/2 font-normal">
 				<MainSection />
