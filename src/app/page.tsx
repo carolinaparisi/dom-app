56ebbd1c1b06eaeef5407a77a3155d6fda58bc6a
Cássio Freitas <cassio.rsfreitas@gmail.com>
2024-08-31 21:35:26 +0100

    fix: replace h-screen with h-dvh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,15 @@ import MainSection from "./components/MainSection";
 import Button from "./components/Button";
 
 export default function Home() {
-	return (
-		<div className="h-screen max-h-screen">
-			<div className="h-1/2 relative">
-				<Image alt="" src={background} fill objectFit="cover" />
-			</div>
-			<div className="flex flex-col justify-between p-5 h-1/2 font-normal">
-				<MainSection />
-				<Button text={"Get started"} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="h-dvh">
+      <div className="h-1/2 relative">
+        <Image alt="" src={background} fill objectFit="cover" />
+      </div>
+      <div className="flex flex-col justify-between p-5 h-1/2 font-normal">
+        <MainSection />
+        <Button text={"Get started"} />
+      </div>
+    </div>
+  );
 }
