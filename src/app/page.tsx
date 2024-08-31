900a43bf204554e57881d99f6e43f871a7d39c00
Carolina Parisi <carolinaparisi5@gmail.com>
2024-08-31 20:57:40 +0100

    fix: remove extra space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Button from "./components/Button";
 export default function Home() {
 	return (
 		<div className="h-screen">
-			<div className=" h-1/2 relative">
+			<div className="h-1/2 relative">
 				<Image alt="" src={background} fill />
 			</div>
 			<div className="flex flex-col justify-between p-5 h-1/2 font-normal">
