46e6df400d35576a39f7a6fdc518a547c3c87479
Carolina Parisi <carolinaparisi5@gmail.com>
2024-09-05 12:49:51 +0100

    feat: add Login page background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+import loginBackground from '../../../public/assets/login-background.jpeg';
+
 export default function Login() {
-  return <h1>LOGIN PAGE</h1>;
+  return (
+    <div className="h-dvh">
+      <div className="flex h-1/6 items-center px-6 text-4xl font-medium text-black">
+        DOM
+      </div>
+      <div className="relative h-5/6 bg-gray">
+        <Image alt="" src={loginBackground} fill className="object-cover" />
+      </div>
+    </div>
+  );
 }
