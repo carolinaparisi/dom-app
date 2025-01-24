a68b738fc4b87203c40ca37be3165ae63628489e
Carolina Parisi <carolinaparisi5@gmail.com>
2025-01-24 13:06:26 +0000

    test: SignInForm component

--- src/components/SignInForm.test.tsx
+++ src/components/SignInForm.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import SignInForm from './SignInForm';
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 jest.mock('firebase/auth', () => ({
   getAuth: jest.fn(),
+  signInWithEmailAndPassword: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
 }));
 
 jest.mock('firebase/database', () => ({
@@ -10,15 +17,7 @@ jest.mock('firebase/database', () => ({
 }));
 
 jest.mock('next/navigation', () => ({
-  useRouter: () => ({
-    push: jest.fn(),
-    events: {
-      on: jest.fn(),
-      off: jest.fn(),
-      emit: jest.fn(),
-    },
-    isFallback: false,
-  }),
+  useRouter: jest.fn(),
 }));
 
 describe('SignInForm component', () => {
@@ -46,5 +45,57 @@ describe('SignInForm component', () => {
     const signUpButton = screen.getByText('Sign Up');
     expect(signInButton).toBeInTheDocument();
     expect(signUpButton).toBeInTheDocument();
+  });
+
+  it('should redirect to home page after successful sign in', async () => {
+    const mockPush = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      push: mockPush,
+    });
+
+    const signInFnMock = signInWithEmailAndPassword as jest.Mock;
+    signInFnMock.mockResolvedValueOnce({});
+
+    render(<SignInForm testId="sign-in-form" />);
+
+    const emailInput = screen.getByPlaceholderText(/your email/i);
+    const passwordInput = screen.getByPlaceholderText(/your password/i);
+    const signInButton = screen.getByText('Sign In');
+
+    fireEvent.change(emailInput, { target: { value: 'user@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+    fireEvent.click(signInButton);
+
+    await waitFor(() => {
+      expect(signInFnMock).toHaveBeenCalled();
+      expect(mockPush).toHaveBeenCalledWith('/');
+    });
+  });
+
+  it('should redirect to home page after successful sign up', async () => {
+    const mockPush = jest.fn();
+    (useRouter as jest.Mock).mockReturnValue({
+      push: mockPush,
+    });
+
+    const signUpFnMock = createUserWithEmailAndPassword as jest.Mock;
+    signUpFnMock.mockResolvedValueOnce({});
+
+    render(<SignInForm testId="sign-in-form" />);
+
+    const emailInput = screen.getByPlaceholderText(/your email/i);
+    const passwordInput = screen.getByPlaceholderText(/your password/i);
+    const signUpButton = screen.getByText('Sign Up');
+
+    fireEvent.change(emailInput, { target: { value: 'user@gmail.com' } });
+    fireEvent.change(passwordInput, { target: { value: '123456' } });
+
+    fireEvent.click(signUpButton);
+
+    await waitFor(() => {
+      expect(signUpFnMock).toHaveBeenCalled();
+      expect(mockPush).toHaveBeenCalledWith('/');
+    });
   });
 });
